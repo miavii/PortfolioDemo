8bb78c25affb7a47229adc0b4d33d3107ead607c
miavii <beachpanda@hotmail.com>
2020-07-29 19:46:48 +0300

    Added new icons to menu

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import HomeIcon from "./../../icons/home.png"
+import GalleryIcon from "./../../icons/code.png"
+import ResumeIcon from "./../../icons/clip.png"
+import AboutIcon from "./../../icons/about.png"
 import './SideMenu.css';
 
 function SideMenu() {
   return (
     <div className="SideMenu">
-      <a href="#">home</a>
-      <a href="#">gallery</a>
-      <a href="#">resume</a>
-      <a href="#">about</a>
+      <a className="home" href="#">
+        <img className="icon" src={HomeIcon}/>
+        <span className="text">home</span></a>
+      <a className="gallery" href="#">
+        <img className="icon" src={GalleryIcon}/>
+        <span className="text">gallery</span></a>
+      <a className="resume" href="#">
+        <img className="icon" src={ResumeIcon}/>
+        <span className="text">resume</span></a>
+      <a className="about" href="#">
+        <img className="icon" src={AboutIcon}/>
+        <span className="text">about</span></a>
     </div>
   );
 }
